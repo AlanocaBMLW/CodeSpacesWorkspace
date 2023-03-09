7d0d300883d8adba66f49a101dd4d3cbb7588da6
AlanocaBMLW <40006552+AlanocaBMLW@users.noreply.github.com>
2023-03-09 04:16:30 +0000

    Ingresando archivo Python

---
+++ Python_Workspace/ejercicio1_4.py
@@ -0,0 +1,11 @@
+""""
+Escriba un programa en Python usando Numpy que devuelva la letra o letras que faltan de un array de letras crecientes (superiores o inferiores). Suponga que siempre faltará al menos una letra en el array.
+
+Ejemplo 1:
+
+Ingrese los elementos de su array unidimensional separado por comas:
+
+Entrada: q, t, u, v, w
+
+Salida: r, s
+"""
--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # CodeSpacesWorkspace
+esto es un workspace para trabajos de programacion remota de diferentes tipos de lenguajes.
